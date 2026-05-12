07a34a9641280924efa8bd94bb6cc27ea9b2a5a0
ItsMeEndemanset <bruhmoments133417@gmail.com>
2026-05-12 23:37:14 +0700

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -3,5 +3,5 @@ $servername = "fishgdps1-fishgdps.h.aivencloud.com";
 $port = 23109;
 $username = "avnadmin";
 $password = "AVNS_M7WKTMF28g5byek3dMA";
-$dbname = "defaultdb";
+$dbname = "gdps_fgdpsrp";
 ?>
